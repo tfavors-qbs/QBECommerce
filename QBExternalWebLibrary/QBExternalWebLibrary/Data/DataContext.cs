c5cb6d9087f4d25c57d39c7e601720d4bec8c7bd
tfavors <tfavor1@gmail.com>
2025-05-08 09:23:27 -0500

    Removing old unneeded commented code

--- QBExternalWebLibrary/QBExternalWebLibrary/Data/DataContext.cs
+++ QBExternalWebLibrary/QBExternalWebLibrary/Data/DataContext.cs
@@ -29,52 +29,6 @@ namespace QBExternalWebLibrary.Data
 
         protected override void OnModelCreating(ModelBuilder modelBuilder) {
             base.OnModelCreating(modelBuilder);
-            //Add Specs
-            //modelBuilder.Entity<Spec>().HasData(
-            //    new Spec { Id = 1, Name = "~A194", DisplayName = "~A194" });
-
-            ////Add Threads
-            //modelBuilder.Entity<Thread>().HasData(
-            //    new Thread { Id = 1, Name = "UNC", DisplayName = "UNC" });
-
-            ////Add Coatings
-            //modelBuilder.Entity<Coating>().HasData(
-            //    new Coating { Id = 1, Name = "PLAIN", DisplayName = "Plain" });
-
-            ////Add Materials
-            //modelBuilder.Entity<Material>().HasData(
-            //    new Material { Id = 1, Name = "2H", DisplayName = "2-H" });
-
-            ////Add Shapes
-            //modelBuilder.Entity<Shape>().HasData(
-            //    new Shape { Id = 1, Name = "HVHX", DisplayName = "HeavyHx" });
-
-            ////Add Classes
-            //modelBuilder.Entity<Class>().HasData(
-            //    new Class { Id = 1, LegacyId = "N", Name = "Nuts", DisplayName = "Nuts" });
-
-            ////Add Groups
-            //modelBuilder.Entity<Group>().HasData(
-            //    new Group { Id = 1, ClassId = 1, LegacyId = "Y", Name = "HeavyHx", DisplayName = "HeavyHx" });
-
-            ////Add ProductId
-            //modelBuilder.Entity<ProductID>().HasData(
-            //    new ProductID { Id = 1, GroupId = 1, ShapeId = 1, MaterialId = 1, CoatingId = 1, ThreadId = 1, SpecId = 1, LegacyId = 5, LegacyName = "NY005" });
-
-            //modelBuilder.Entity<Diameter>().HasData(
-            //    new Diameter { Id = 1, Name = "0.7500", DisplayName = "3/4\"", Value = .75 });
-
-            //modelBuilder.Entity<SKU>().HasData(
-            //    new SKU { Id = 1, DiameterId = 1, ProductIDId = 1, Name = "5VK" });
-
-            //modelBuilder.Entity<Length>().HasData(
-            //    new Length { Id = 1, Name = "1.00", DisplayName = "1.00", Value = 1.00 });
-
-            //modelBuilder.Entity<Client>().HasData(
-            //    new Client { Id = 1, Name = "TestClient", LegacyId = "6202"});
-
-            //modelBuilder.Entity<ContractItem>().HasData(
-            //    new ContractItem { Id = 1, CustomerStkNo = "TestContractItem", Description = "TestContractItem", Price = 25.00M, ClientId = 1, NonStock = false, SKUId = 1 });
         }
     }
 
